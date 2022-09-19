ced1ca221bdb72918d3852a541a5f2f032295e27
manhhung2801 <hung28012002zz@gmail.com>
2022-09-19 23:31:39 +0700

    insert database table php

---
+++ insert.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Insert </title>
+</head>
+<body>
+    <div style="text-align:center;">
+         <h1>Insert Database Table</h1>
+        <form action="process.php" method="post">
+            First Name<br>
+            <input type="text" name="first_name">
+            <br>
+            Last Name:<br>
+            <input type="text" name="last_name">
+            <br>
+            City Name:<br>
+            <input type="text" name="city_name">
+            <br>
+            Email Id:<br>
+            <input type="email" name="email">
+            <br><br>
+            <input type="submit" name="save" value="submit">
+        </form>
+   </div>
+</body>
+</html>
